a4c01d02839967dad630be7f94adc9f16d34a85c
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-17 21:30:31 +0530

    Modified: generic method to find max of given parameters

--- src/com/generics/GenericsMaximum.java
+++ src/com/generics/GenericsMaximum.java
@@ -4,9 +4,9 @@ package com.generics;
  * GenericsMaximum Class uses generics concepts
  */
 public class GenericsMaximum {
-    // method takes three Integer parameters and returns max of them
-    static private Integer maxOfThree(Integer a,Integer b,Integer c){
-        Integer max=a;
+    // method takes three generic parameters and returns max of them
+    static private <T extends Comparable> T maxOfThree(T a,T b,T c){
+        T max=a;
         if(b.compareTo(max)>0 ){
             max= b;
         }
@@ -14,28 +14,6 @@ public class GenericsMaximum {
             max= c;
         }
         return max;
-    }
-    // method takes three Float parameters and returns max of them
-    static private Float maxOfThree(Float a,Float b,Float c){
-        Float max=a;
-        if(b.compareTo(max)>0 ){
-            max= b;
-        }
-        if(c.compareTo(max)>0 ){
-            max= c;
-        }
-        return max;
-    }
-    // method to find max of three strings
-    static private void maxOfThree(String a,String b, String c){
-        String max=a;
-        if(b.compareTo(max)>0 ){
-            max= b;
-        }
-        if(c.compareTo(max)>0 ){
-            max= c;
-        }
-        System.out.println(max);
     }
 
     // Generic methods used
@@ -43,10 +21,14 @@ public class GenericsMaximum {
         //welcome message
         System.out.println("Generics");
         //variables
-        //float a=1.1f,b=2.2f,c=3.3f;
+        int n1=1,n2=2,n3=3;
+        float num1=1.1f,num2=2.2f,num3=3.3f;
         String a="Apple",b="Banana",c="Peach";
+        // max of integer number given
+        System.out.println("Max of String: 1, 2, 3 is "+maxOfThree(n1,n2,n3));
+        // max of float number given
+        System.out.println("Max of String: 1.1, 2.2, 3.3 is "+maxOfThree(num1,num2,num3));
         // max of parameter given
-        System.out.println("Max of String: Apple, Banana, Peach is ");
-        maxOfThree(a,b,c);
+        System.out.println("Max of String: Apple, Banana, Peach is "+maxOfThree(a,b,c));
     }
 }
