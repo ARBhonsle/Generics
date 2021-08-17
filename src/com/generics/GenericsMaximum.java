1345057a7ddecfa39c4d51d1cdf56a57124c4967
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-17 20:50:38 +0530

    Added: Project files

---
+++ src/com/generics/GenericsMaximum.java
@@ -0,0 +1,11 @@
+package com.generics;
+
+/**
+ * GenericsMaximum Class uses generics concepts
+ */
+public class GenericsMaximum {
+    // Generic methods used
+    public static void main(String[] args) {
+        System.out.println("Generics");
+    }
+}
