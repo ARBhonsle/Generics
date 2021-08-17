b344ad30b646cb8cc4e2af65e27bc997922b6c83
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-17 21:09:00 +0530

    Added: method to find max of three integers

--- src/com/generics/GenericsMaximum.java
+++ src/com/generics/GenericsMaximum.java
@@ -4,8 +4,25 @@ package com.generics;
  * GenericsMaximum Class uses generics concepts
  */
 public class GenericsMaximum {
+    // method takes three Integer parameters and returns max of them
+    static private Integer maxOfThree(Integer a,Integer b,Integer c){
+        Integer max=a;
+        if(b.compareTo(max)>0 ){
+            max= b;
+        }
+        if(c.compareTo(max)>0 ){
+            max= c;
+        }
+        return max;
+    }
+
     // Generic methods used
     public static void main(String[] args) {
+        //welcome message
         System.out.println("Generics");
+        //variables
+        int a=1,b=2,c=3;
+        // max of numbers given
+        System.out.println("Max of Numbers 1, 2, 3 is "+maxOfThree(1,2,3));
     }
 }
